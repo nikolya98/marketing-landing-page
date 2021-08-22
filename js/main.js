94d0d5f3603919d1dbb7f8ceb6cfc0812cc91c5b
nikolya98 <nikolya.knyazev.98@mail.ru>
2021-08-22 14:07:39 +0300

    Make 2 sections yet

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function () {
   var containerEl = document.querySelector('.portfolio__list');
   var mixer = mixitup(containerEl);
+
+  $('.featured__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg" alt="Кнопка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg" alt="Кнопка вправо"></button>',
+  });
 });

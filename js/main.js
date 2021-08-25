16a253e7e4658f540bca0074ac9ac79052fe8c81
nikolya98 <nikolya.knyazev.98@mail.ru>
2021-08-25 20:16:36 +0300

    Make adaptive version for 1-4 sections

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(function () {
-  var containerEl = document.querySelector('.portfolio__list');
-  var mixer = mixitup(containerEl);
-
   $('.featured__slider').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg" alt="Кнопка влево"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg" alt="Кнопка вправо"></button>',
   });
+
+  $(function () {
+    $('.header__menu-btn').on('click', function () {
+      $('.header__menu').toggleClass('header__menu--active');
+    });
+  });
+
+  var containerEl = document.querySelector('.portfolio__list');
+  var mixer = mixitup(containerEl);
 });

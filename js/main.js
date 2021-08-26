18750e6a6b4d89804fbaf1f78c077d6c2144864f
nikolya98 <nikolya.knyazev.98@mail.ru>
2021-08-26 16:27:19 +0300

    Complete adaptive version of layout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ $(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg" alt="Кнопка влево"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg" alt="Кнопка вправо"></button>',
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          arrows: false,
+        }
+      },
+    ],
   });
+
+
 
   $(function () {
     $('.header__menu-btn').on('click', function () {
